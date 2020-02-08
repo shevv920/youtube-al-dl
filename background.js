c629b2566a5eec6a31807361c4e4d050073260cb
Vladimir Shevtsov <shevvv@tutanota.com>
2020-02-08 14:21:44 +0300

    feature: stop start download

--- background.js
+++ background.js
@@ -1,52 +1,62 @@
 let currentId = undefined;
 let port = undefined;
+let downloadCancelled = false;
+let downloadTargets = [];
+chrome.downloads.onChanged.addListener(onChanged);
+
 chrome.runtime.onConnect.addListener(p => {
   if(p.name === 'yald')
     port = p;
     port.onMessage.addListener(msg => {
-      if (msg.action === "start" && msg.items)
-        downloadSequentially(msg.items);
-      else if(msg.action === "stop")
+      if (msg.action === "start" && msg.items) {
+        downloadTargets = msg.items;
+        downloadCancelled = false;
+        downloadSequentially();
+      } else if(msg.action === "stop")
         stopDownload();
     });
 });
 
 function sendMessage(msg) {
   if(port !== undefined) {
-    port.postMessage(msg);
+    port.postMessage({action: msg});
   }
 }
 
 function stopDownload() {
-  if(currentId !== undefined) chrome.downloads.cancel(currentId, () => sendMessage("stopped"));
+  if(currentId !== undefined) chrome.downloads.cancel(currentId);
+  downloadCancelled = true;
 }
 
-function downloadSequentially(downloadTargets) {
-  if(!downloadTargets) return;   
+function onChanged({id, state}) {
+  if (id === currentId && state) {
+    switch(state.current) {
+      case 'complete':                           
+        chrome.storage.local.set({[item.title]: "completed"}, () =>{
+          if(downloadCancelled === true) {
+            sendMessage("stopped");
+           } else {
+            downloadSequentially(downloadTargets);
+           }
+        });        
+        break;
+      case 'interrupted': 
+        sendMessage("stopped");
+        break;
+      case 'in_progress': 
+        break;
+    }
+  }
+}
 
+function downloadSequentially() {
+  if(!downloadTargets) return;   
   item = downloadTargets.shift();
   chrome.storage.local.get([item.title], items => {
     if(items[item.title] !== "completed") {
       chrome.downloads.download({url: item.url, filename: item.fileName}, cId => { 
         sendMessage("started");
-        currentId = cId;
-        function onChanged({id, state}) {
-          if (id === currentId && state) {
-            switch(state.current) {
-              case 'complete':
-                chrome.downloads.onChanged.removeListener(onChanged);                             
-                chrome.storage.local.set({[item.title]: "completed"})
-                downloadSequentially(downloadTargets);
-                break;
-              case 'interrupted': 
-                sendMessage("stopped");
-                break;
-              case 'in_progress': 
-                break;
-            }
-          }
-        }
-        chrome.downloads.onChanged.addListener(onChanged)
+        currentId = cId;       
       });  
     } else {
       downloadSequentially(downloadTargets);
